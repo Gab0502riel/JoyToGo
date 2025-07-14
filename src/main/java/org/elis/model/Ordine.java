90468cb263b30bab3ed4c1e105ac8458b0adf390
Raffinoo <raffaele.recupero@gmail.com>
2025-07-14 12:45:03 +0200

    Update Ordine.java

--- src/main/java/org/elis/model/Ordine.java
+++ src/main/java/org/elis/model/Ordine.java
@@ -1,5 +1,58 @@
 package org.elis.model;
 
 public class Ordine {
+	private int id;
+    private enum Stato {
+        ATTESA_CONFERMA("In attesa di conferma"),
+        CONFERMATO("Confermato"),
+        RIFIUTATO("Rifiutato"),
+        RITIRATO("Ritirato"),
+        ANNULLATO("Annullato");
 
+        private final String descStato;
+
+        Stato(String descStato) {
+            this.descStato = descStato;
+        }
+
+        public String getDescStato() {
+            return descStato;
+        }
+    }
+	private String r_testo;
+	private int r_voto;
+	
+	public Ordine(int id, String r_testo, int r_voto) {
+		super();
+		this.id = id;
+		this.r_testo = r_testo;
+		this.r_voto = r_voto;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getR_testo() {
+		return r_testo;
+	}
+
+	public void setR_testo(String r_testo) {
+		this.r_testo = r_testo;
+	}
+
+	public int getR_voto() {
+		return r_voto;
+	}
+
+	public void setR_voto(int r_voto) {
+		this.r_voto = r_voto;
+	}
+	
+	
+	
 }
