3655e6228a2723f101fda4e3ea82aa35354fa151
Gab0502riel <gabriel32dantoni@gmail.com>
2025-07-16 12:31:03 +0200

    aggiunto impl commentato utentedao

--- src/main/java/org/elis/dao/UtenteDao.java
+++ src/main/java/org/elis/dao/UtenteDao.java
@@ -7,3 +7,8 @@ public interface UtenteDao extends GeneralDao<Utente> {
 	Utente findByEmailPassword(String email, String password) throws Exception;
 	void insert(Utente utente) throws Exception;
 }
+
+/*	    int salvaUtente(Utente utente) throws Exception;
+Utente findByEmail(String email) throws Exception;
+
+Utente findByEmailAndPassword(String email, String password) throws Exception;*/
